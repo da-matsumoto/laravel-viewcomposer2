72f96dcd63c19886558cbe0f82c5813d417ed8ba
da-matsumoto <da.mats1990@gmail.com>
2018-06-01 13:18:43 +0900

    ビューコンポーザークラスを作成

--- viewcomposer/app/Http/Controllers/HelloController.php
+++ viewcomposer/app/Http/Controllers/HelloController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     public function index(){
-        return view('hello.index', ['message'=>'Hello!']);
+        return view('hello.index', ['message'=>'hello!']);
     }
 }
